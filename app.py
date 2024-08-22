8f326712d7565b1ec2024898059944b8727f9332
Amit Kumar Mondal <mamit5620@gmail.com>
2024-08-22 14:46:17 +0530

    update some files

--- app.py
+++ app.py
@@ -50,5 +50,4 @@ def download_file(filename):
     path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     return send_file(path, as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
